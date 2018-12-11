aff8e1b5305ff555ea501da9d6c780bc2f4e5821
Fernando de Almeida Coelho <nandoacoelho@gmail.com>
2018-12-11 11:02:34 -0200

    Fix behavior when selecting multiple scheduled deliveries

--- tests/fixtures/logisticsInfo-scheduled-delivery.js
+++ tests/fixtures/logisticsInfo-scheduled-delivery.js
@@ -267,7 +267,157 @@ const SCHEDULED_DELIVERY_LOGISTICS_INFO = [
   },
 ]
 
+const MULTIPLE_MANDATORY_SCHEDULED_DELIVERY_LOGISTICS_INFO = [
+  {
+    itemIndex: 0,
+    selectedSla: 'outra agendada',
+    selectedDeliveryChannel: '',
+    addressId: '62e1db5500824a66bcef708d09388a8e',
+    slas: [
+      {
+        id: 'agendada',
+        deliveryChannel: 'delivery',
+        name: 'agendada',
+        deliveryIds: [
+          {
+            courierId: '1',
+            warehouseId: '1afe1cf',
+            dockId: '1b91c3b',
+            courierName: 'Courrier e-commerce',
+            quantity: 1,
+          },
+        ],
+        shippingEstimate: '0bd',
+        shippingEstimateDate: null,
+        lockTTL: null,
+        availableDeliveryWindows: [{}, {}],
+        deliveryWindow: null,
+        price: 334,
+        listPrice: 334,
+        tax: 0,
+        pickupStoreInfo: {
+          isPickupStore: false,
+          friendlyName: null,
+          address: null,
+          additionalInfo: null,
+          dockId: null,
+        },
+      },
+      {
+        id: 'outra agendada',
+        deliveryChannel: 'delivery',
+        name: 'outra agendada',
+        deliveryIds: [
+          {
+            courierId: '1',
+            warehouseId: '1afe1cf',
+            dockId: '1b91c3b',
+            courierName: 'Courrier e-commerce',
+            quantity: 1,
+          },
+        ],
+        shippingEstimate: '0bd',
+        shippingEstimateDate: null,
+        lockTTL: null,
+        availableDeliveryWindows: [{}, {}],
+        deliveryWindow: null,
+        price: 333,
+        listPrice: 333,
+        tax: 0,
+        pickupStoreInfo: {
+          isPickupStore: false,
+          friendlyName: null,
+          address: null,
+          additionalInfo: null,
+          dockId: null,
+        },
+      },
+    ],
+    shipsTo: ['BRA'],
+    itemId: '100006785',
+    deliveryChannels: [
+      {
+        id: 'delivery',
+      },
+    ],
+  },
+  {
+    itemIndex: 1,
+    selectedSla: 'outra agendada',
+    selectedDeliveryChannel: '',
+    addressId: '62e1db5500824a66bcef708d09388a8e',
+    slas: [
+      {
+        id: 'agendada',
+        deliveryChannel: 'delivery',
+        name: 'agendada',
+        deliveryIds: [
+          {
+            courierId: '1',
+            warehouseId: '1afe1cf',
+            dockId: '1b91c3b',
+            courierName: 'Courrier e-commerce',
+            quantity: 1,
+          },
+        ],
+        shippingEstimate: '0bd',
+        shippingEstimateDate: null,
+        lockTTL: null,
+        availableDeliveryWindows: [{}, {}],
+        deliveryWindow: null,
+        price: 333,
+        listPrice: 333,
+        tax: 0,
+        pickupStoreInfo: {
+          isPickupStore: false,
+          friendlyName: null,
+          address: null,
+          additionalInfo: null,
+          dockId: null,
+        },
+      },
+      {
+        id: 'outra agendada',
+        deliveryChannel: 'delivery',
+        name: 'outra agendada',
+        deliveryIds: [
+          {
+            courierId: '1',
+            warehouseId: '1afe1cf',
+            dockId: '1b91c3b',
+            courierName: 'Courrier e-commerce',
+            quantity: 1,
+          },
+        ],
+        shippingEstimate: '0bd',
+        shippingEstimateDate: null,
+        lockTTL: null,
+        availableDeliveryWindows: [{}, {}],
+        deliveryWindow: null,
+        price: 333,
+        listPrice: 333,
+        tax: 0,
+        pickupStoreInfo: {
+          isPickupStore: false,
+          friendlyName: null,
+          address: null,
+          additionalInfo: null,
+          dockId: null,
+        },
+      },
+    ],
+    shipsTo: ['BRA'],
+    itemId: '100006786',
+    deliveryChannels: [
+      {
+        id: 'delivery',
+      },
+    ],
+  },
+]
+
 module.exports = {
   MANDATORY_SCHEDULED_DELIVERY_LOGISTICS_INFO,
   SCHEDULED_DELIVERY_LOGISTICS_INFO,
+  MULTIPLE_MANDATORY_SCHEDULED_DELIVERY_LOGISTICS_INFO,
 }
